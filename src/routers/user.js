3d1ac7278224c949c19dd81e8cc42f23eb696bd4
andres_vichuco@hotmail.com <andres_vichuco@hotmail.com>
2021-06-18 17:31:24 -0500

    creacion nuevo archivo grifs.js

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = new express.Router()
 const User = require('../models/user')
-const gfs = require('../db/gridfs')
 const bodyParser = require('body-parser')
 const multer = require('multer')
 const xlsxj = require("xlsx-to-json");
