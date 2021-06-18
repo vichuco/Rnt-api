a0f7694f9ee3c6854a586442e873491db0e2b436
andres_vichuco@hotmail.com <andres_vichuco@hotmail.com>
2021-06-17 22:27:51 -0500

    crear conexion dentro de la clase mongoose

--- src/routers/user.js
+++ src/routers/user.js
@@ -12,6 +12,7 @@ const GridFsStorage = require('multer-gridfs-storage')
 const Grid = require('gridfs-stream')
 const path = require('path')
 const iconvlite = require('iconv-lite')
+const gfs = require('./db/mongoose')
 const mongoose = require('mongoose')
 const rimraf = require("rimraf")
 const fetch = require("node-fetch")
@@ -103,31 +104,8 @@ router.post('/login', urlencodedParser, async (req, res) => {
         //req.session.userInfo = ({ token  })
         const conn = mongoose.createConnection(process.env.MONGODB_URL);
 
-        conn.once('open', () => {
-            // Init stream
-            gfs = Grid(conn.db, mongoose.mongo);
-            gfs.collection('uploads');
-            gfs.find().toArray((err, files) => {
-                // Check if files
-                if (!files || files.length === 0) {
-                    res.render('audio.ejs', { files: false });
-                } else {
-                    files.map(file => {
-                        if (
-                            file.contentType === 'video/mp4'
-                        ) {
-                            file.isImage = true;
-                        } else {
-                            file.isImage = false;
-                        }
-                    });
-                    res.render('audio.ejs', { files: files });
-                }
-            })
 
-        })
-
-        /*gfs.find().toArray((err, files) => {
+        gfs.find().toArray((err, files) => {
             // Check if files
             if (!files || files.length === 0) {
                 res.render('audio.ejs', { files: false });
@@ -143,7 +121,7 @@ router.post('/login', urlencodedParser, async (req, res) => {
                 });
                 res.render('audio.ejs', { files: files });
             }
-        })*/
+        })
     } catch (e) {
         res.status(400).send("usuario o contrasena incorrectas")
     }
