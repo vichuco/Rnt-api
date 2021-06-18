44415a9ab69726d4ce97f4c58c60dda7b106b4d9
andres_vichuco@hotmail.com <andres_vichuco@hotmail.com>
2021-06-18 16:33:41 -0500

    creacion nuevo archivo grifs.js

--- src/db/gridfs.js
+++
@@ -1,7 +0,0 @@
-const conn = mongoose.createConnection(process.env.MONGODB_URL);
-conn.once('open', () => {
-    // Init stream
-    gfs = Grid(conn.db, mongoose.mongo);
-    gfs.collection('uploads');
-    
-})
