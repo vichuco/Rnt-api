34ca7663bb8a465e4e5ebd4b0391f6923d2bb90d
andres_vichuco@hotmail.com <andres_vichuco@hotmail.com>
2021-06-21 23:10:49 -0500

    prueba de gfs como uso de la misma conexion en el documento monngose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose')
 const Grid = require('gridfs-stream')
 
-const conn= mongoose.connect(process.env.MONGODB_URL, {
+/*mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+
+})*/
+const conn = mongoose.createConnection(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
 
 })
-//const conn = mongoose.createConnection(process.env.MONGODB_URL);
 conn.once('open', () => {
     // Init stream
     const gfs = Grid(conn.db);
