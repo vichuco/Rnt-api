7346578d5fcc39dd565f134e8b89c9268b52c202
andres_vichuco@hotmail.com <andres_vichuco@hotmail.com>
2021-06-26 20:57:51 -0500

    creacion de otra base de datos en atlas cluster y otra direcciond e conexion para heroku

---
+++ src/db/mongooseUpload.js
@@ -0,0 +1,8 @@
+const conn = mongoose.createConnection(process.env.MONGODB_URL2);
+const gfs = null
+conn.once('open', () => {
+    // Init stream
+    gfs = Grid(conn.db);
+    gfs.collection('uploads');
+})
+module.exports = exports = mongoose;
