4b8f22ddac4125389f29baa80dc07f9809193262
andres_vichuco@hotmail.com <andres_vichuco@hotmail.com>
2021-06-26 21:52:10 -0500

    creacion de otra base de datos en atlas cluster y otra direcciond e conexion para heroku

--- src/routers/user.js
+++ src/routers/user.js
@@ -118,24 +118,31 @@ router.post('/login', urlencodedParser, async (req, res) => {
         //res.send({ user, token })
         // res.setHeader('Authorization', 'Bearer '+ token)
         //req.session.userInfo = ({ token  })
-
-        gfs.find().toArray((err, files) => {
-            // Check if files
-            if (!files || files.length === 0) {
-                res.render('audio.ejs', { files: false });
-            } else {
-                files.map(file => {
-                    if (
-                        file.contentType === 'video/mp4'
-                    ) {
-                        file.isImage = true;
-                    } else {
-                        file.isImage = false;
-                    }
-                });
-                res.render('audio.ejs', { files: files });
-            }
+        const conn = mongoose.createConnection(process.env.MONGODB_URL);
+        conn.once('open', () => {
+            // Init stream
+            gfs = Grid(conn.db);
+            gfs.collection('uploads');
+            gfs.find().toArray((err, files) => {
+                // Check if files
+                if (!files || files.length === 0) {
+                    res.render('audio.ejs', { files: false });
+                } else {
+                    files.map(file => {
+                        if (
+                            file.contentType === 'video/mp4'
+                        ) {
+                            file.isImage = true;
+                        } else {
+                            file.isImage = false;
+                        }
+                    });
+                    res.render('audio.ejs', { files: files });
+                }
+            })
         })
+
+        
 
 
 
