c47f5c9aa1cf08db771913c1caf9b8237e6ff2f2
andres_vichuco@hotmail.com <andres_vichuco@hotmail.com>
2021-06-29 18:07:53 -0500

    solucion final pruebas de creacion de parrilla

--- src/models/archivoJson.js
+++ src/models/archivoJson.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
-const archivoSchema = new mongoose.Schema({
+const archivoSchema = new Schema({
 
     cualquiera: Schema.Types.Mixed
 })
 
-const Archivo = mongoose.model('Jsons', archivoSchema)
+const Archivo = mongoose.model('Archivo', archivoSchema)
 
 module.exports = Archivo
