57cfda4fc0c8cd81e1804cf4a55eeadb4d45aab3
andres_vichuco@hotmail.com <andres_vichuco@hotmail.com>
2021-06-29 19:27:40 -0500

    solucion final pruebas de creacion de parrilla

--- src/models/archivoJson.js
+++ src/models/archivoJson.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose')
 
 const Mixed = mongoose.Schema.Types.Mixed;
+
 const archivoSchema = new mongoose.Schema({
 
     cualquiera: Mixed
 })
-
+archivoSchema.set('toJSON', { getters: true, virtuals: false });
 archivoSchema.methods.searchJson = async function () {
-    const user = await Archivo.find({name:'videos'})
-
-    return user
+    const user = await Archivo.find({})
+    const userJson = user.toJSON()
+    return userJson
 }
 
 const Archivo = mongoose.model('Archivo', archivoSchema)
